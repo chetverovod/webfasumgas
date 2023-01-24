dadaf7e783591f74f109a09b04cab00e72ba936a
chetverovod <1526432+chetverovod@users.noreply.github.com>
2023-01-24 11:57:22 +0300

    Update test_webfasumgaz.py
    Functions descriptions were added.

--- test_webfasumgaz.py
+++ test_webfasumgaz.py
@@ -6,14 +6,20 @@ from webfasumgaz import app
 
 client = TestClient(app)
 
-
+#-----------------------------------------------------------------
 def test_read_webfasumgaz():
+     """Test checks handling of empty GET request.
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message":  "Webfasumgaz application is online!"}
 
-
+#-----------------------------------------------------------------
 def test_read_empty():
+    """Test checks handling of empty arg by prediction function.
+    It sends to service empty string, after that it checks status 
+    and answer equation to string 'Error: text is absent.'
+    """
     response = client.post("/predict/",
         json={"text": ""}
     )
@@ -22,8 +28,12 @@ def test_read_empty():
     assert response.status_code == 200
     assert json_data == 'Error: text is absent.'
 
-
+#-----------------------------------------------------------------
 def test_read_text():
+    """Test checks prediction. It sends to service reference text
+    from file "text1.txt" and compare responce with reference 
+    string from file "answer1.txt". 
+    """
     f = open("texts/text1.txt")
     fd = f.read()
     f.close()
